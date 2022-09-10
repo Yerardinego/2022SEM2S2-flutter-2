d7a10bf9cf1be63a4ccb6e525cd866d17fa4f529
Yerardinego <yerardine.sua@unillanos.edu.com>
2022-09-10 16:33:29 -0500

    actividad IMC

---
+++ lib/pages/detalle.page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class DetallePage extends StatelessWidget {
+  const DetallePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Detalle del calculo"),
+      ),
+    );
+  }
+}
